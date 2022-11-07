f7e0a51e307469630ebe8d57bd141e92cfd3ea1f
Danil M <malaxoff.dan@gmail.com>
2022-11-07 08:22:01 +0700

    First lab on the Interface man - electronic computers

---
+++ Interface_lab№1/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Interface_lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.перваяПодтемаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.втораяПодтемаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(793, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.перваяПодтемаToolStripMenuItem,
+            this.втораяПодтемаToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(89, 20);
+            this.toolStripMenuItem1.Text = "Ввод данных";
+            // 
+            // перваяПодтемаToolStripMenuItem
+            // 
+            this.перваяПодтемаToolStripMenuItem.Name = "перваяПодтемаToolStripMenuItem";
+            this.перваяПодтемаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.перваяПодтемаToolStripMenuItem.Text = "Первая подтема";
+            this.перваяПодтемаToolStripMenuItem.Click += new System.EventHandler(this.перваяПодтемаToolStripMenuItem_Click);
+            // 
+            // втораяПодтемаToolStripMenuItem
+            // 
+            this.втораяПодтемаToolStripMenuItem.Name = "втораяПодтемаToolStripMenuItem";
+            this.втораяПодтемаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.втораяПодтемаToolStripMenuItem.Text = "Вторая подтема";
+            this.втораяПодтемаToolStripMenuItem.Click += new System.EventHandler(this.втораяПодтемаToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кол-во испытаний N";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вероятность ошибки Pi";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0,2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Вероятность выбора 1-й подтемы";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(336, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0,5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(518, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(336, 121);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(333, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Общее среднее время";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(167, 121);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(164, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Среднее время 2-й подтемы";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(15, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Среднее время 1-й подтемы";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(0, 147);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Histogram";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(791, 331);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 479);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Interface_lab№1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem перваяПодтемаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem втораяПодтемаToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Interface_lab№1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface_lab_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SubTheme subTheme1;
+        private SubTheme subTheme2;
+
+        private struct SubTheme
+        {
+            public SubTheme(List<Route> routes, bool number_of_subtheme)
+            {
+                this.routes = routes;
+                this.number_of_subtheme = number_of_subtheme;
+            }
+
+            public List<Route> routes;
+            public bool number_of_subtheme;
+
+        }
+
+        private struct Route
+        {
+            public Route(List<int> steps, double prob)
+            {
+                this.steps = steps;
+                this.prob_of_route = prob;
+            }
+
+            public List<int> steps;
+            public double prob_of_route;
+        }
+
+        private void ReadDataFromCSV(bool number_of_subtheme)
+        {
+            List<Route> routes = new List<Route>();
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "csv File|*.csv";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = System.IO.File.ReadAllLines(opf.FileName);
+                if(lines.Length == 0)
+                {
+                    MessageBox.Show("Пустой файл");
+                    return;
+                }
+                try
+                {
+                    int first_root = 0;
+                    foreach (string line in lines)
+                    {
+                        string[] data = line.Split(',');
+                        string prob = data[0].Replace(".", ",");
+                        double prob_of_route = Convert.ToDouble(prob);
+                        List<int> steps = new List<int>();
+                        if (line == lines[0]) first_root = Convert.ToInt32(data[1]);
+                        else if (Convert.ToInt32(data[1]) != first_root)
+                        {
+                            MessageBox.Show("Все маршруты в подтемах должны начинаться с одного и того же номера шага диалога");
+                            return;
+                        }
+                            for (var i = 1; i < data.Length; i++)
+                            steps.Add(Convert.ToInt32(data[i]));
+                        Route route = new Route(steps, prob_of_route);
+                        routes.Add(route);
+                    }
+                    SubTheme subtheme = new SubTheme(routes, number_of_subtheme);
+                    if (!number_of_subtheme)
+                        subTheme1 = subtheme;
+                    else
+                        subTheme2 = subtheme;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("В файле содержатся ошибки");
+                }
+            }
+        }
+
+        private void перваяПодтемаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReadDataFromCSV(false);
+            перваяПодтемаToolStripMenuItem.Text += '*';
+        }
+
+        private void втораяПодтемаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReadDataFromCSV(true);
+            втораяПодтемаToolStripMenuItem.Text += '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(subTheme1.routes == null || subTheme2.routes == null)
+            {
+                MessageBox.Show("Одна или обе подтемы с маршрутами не введены");
+                return;
+            }
+
+            try
+            {
+                int N = Convert.ToInt32(textBox1.Text);
+                double P_err = Convert.ToDouble(textBox2.Text);
+                double P_subtheme1 = Convert.ToDouble(textBox3.Text);
+                double P_subtheme2 = 1 - P_subtheme1;
+                var current_subtheme = new SubTheme();
+                var current_route = new Route();
+                double time1 = 0;
+                double time2 = 0;
+                var k1 = 0;
+                var k2 = 0;
+                List<double> times_of_subthemes = new List<double>();
+                var rand = new Random();
+                for (var i = 0; i < N; i++)
+                {
+                    double time_of_subtheme = 0;
+                    var Generated_P_subtheme = rand.NextDouble();
+                    if (Generated_P_subtheme <= P_subtheme1)
+                        current_subtheme = subTheme1;
+                    else
+                        current_subtheme = subTheme2;
+                    var Generated_P_route = rand.NextDouble();
+                    for (var j = 0; j < current_subtheme.routes.Count; j++)
+                    {
+                        if (Generated_P_route <= current_subtheme.routes[j].prob_of_route)
+                        {
+                            current_route = current_subtheme.routes[j];
+                            break;
+                        }
+                        else
+                            Generated_P_route -= current_subtheme.routes[j].prob_of_route;
+                    }
+                    for (var index = 0; index < current_route.steps.Count; index++)
+                    {
+                        var Generated_time = rand.Next(5, 16) + rand.NextDouble();
+                        if (!current_subtheme.number_of_subtheme)
+                        {
+                            time1 += Generated_time;
+                            time_of_subtheme += Generated_time;
+                        }
+                        else
+                        {
+                            time2 += Generated_time;
+                            time_of_subtheme += Generated_time;
+                        }
+                        if (rand.NextDouble() <= P_err)
+                            index = 0;
+
+                    }
+                    times_of_subthemes.Add(time_of_subtheme);
+                    if (!current_subtheme.number_of_subtheme)
+                        k1++;
+                    else
+                        k2++;
+                }
+                var average_time1 = time1 / k1;
+                var average_time2 = time2 / k2;
+                var average_time = time1 + time2;
+                average_time /= k1 + k2;
+
+                textBox6.Text = average_time1.ToString();
+                textBox5.Text = average_time2.ToString();
+                textBox4.Text = average_time.ToString();
+
+                //построение гистограммы
+                chart1.Series[0].Points.Clear();
+                var K = Convert.ToInt32(1 + 3.22 * Math.Log10(N));
+                double min = double.MaxValue, max = double.MinValue;
+                foreach (var item in times_of_subthemes)
+                {
+                    min = (min > item) ? (item) : (min);
+                    max = (max < item) ? (item) : (max);
+                }
+                double[] intervals = new double[K + 1];
+                for (var i = 0; i < K + 1; i++)
+                    intervals[i] = min + (max - min) / K * i;
+                int[] y = new int[K];
+                double[] x = new double[K];
+                for (var i = 0; i < intervals.Length - 1; i++)
+                {
+                    x[i] = (intervals[i + 1] + intervals[i]) / 2;
+                    foreach (var j in times_of_subthemes)
+                        if (j >= intervals[i] && j < intervals[i + 1])
+                            y[i]++;
+                }
+                for (var i = 0; i < K; i++)
+                    chart1.Series[0].Points.AddXY(x[i], y[i]);
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show("Неправильный ввод параметров");
+            }
+        }
+    }
+}
